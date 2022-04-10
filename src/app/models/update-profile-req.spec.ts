0fe0ceaba3cdf72a3b3a9d77f44ed6ce99ab239c
keithcharlesc <keithcharleschan@gmail.com>
2022-04-10 16:54:13 +0800

    Update Proifle - Customer Service

---
+++ src/app/models/update-profile-req.spec.ts
@@ -0,0 +1,7 @@
+import { UpdateProfileReq } from './update-profile-req';
+
+describe('UpdateProfileReq', () => {
+  it('should create an instance', () => {
+    expect(new UpdateProfileReq()).toBeTruthy();
+  });
+});
