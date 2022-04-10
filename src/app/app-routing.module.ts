2aaf99fc5f3941a98412c5f08c41049337485e9a
Lyn <lyntan2001@outlook.com>
2022-04-10 15:50:17 +0800

    Fix header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccessRightErrorComponent } from './admin/access-right-error/access-right-error.component';
 
 import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
-  { path: 'index', component: IndexComponent }
+  { path: 'index', component: IndexComponent },
+  { path: 'accessRightError', component: AccessRightErrorComponent}
 ];
 
 @NgModule({
