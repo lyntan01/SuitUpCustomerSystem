612468961024c260d780e3e8d456bd0982e86b94
xxianhuii <e0559743@u.nus.edu>
2022-04-14 16:28:46 +0800

    working filter but ugly

--- src/app/components/filter-bar/filter-bar.component.ts
+++ src/app/components/filter-bar/filter-bar.component.ts
@@ -1,15 +1,119 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Category } from 'src/app/models/category';
+import { Tag } from 'src/app/models/tag';
+import { StandardProductService } from 'src/app/services/standard-product.service';
 
 @Component({
   selector: 'app-filter-bar',
   templateUrl: './filter-bar.component.html',
-  styleUrls: ['./filter-bar.component.css']
+  styleUrls: ['./filter-bar.component.css'],
 })
 export class FilterBarComponent implements OnInit {
+  categories: Category[];
+  selectedCategories: Category[] = new Array();
+  selectedPriceRange: number[];
+  tags: Tag[];
+  selectedTags: Tag[] = new Array();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private standardProductService: StandardProductService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.categories = new Array();
+    this.selectedPriceRange = [0, 1000];
+    this.tags = new Array();
   }
 
+  ngOnInit(): void {
+    this.fetchCategories();
+    this.fetchTags();
+    this.setPriceRange();
+  }
+
+  fetchCategories() {
+    this.standardProductService.getCategories().subscribe((respond) => {
+      console.log('CATEGORY: ' + respond);
+      this.categories = respond;
+      // this.activatedRoute.queryParams.subscribe((params) => {
+      //   this.selectedCategories = new Array();
+      //   let urlCategoryIds: number[] | undefined = params['category'];
+      //   console.log('URL CATEGORY: ' + urlCategoryIds);
+      //   if (urlCategoryIds != undefined && !Array.isArray(urlCategoryIds)) {
+      //     urlCategoryIds = new Array(urlCategoryIds);
+      //   }
+      //   urlCategoryIds?.forEach((categoryId) => {
+      //     const temp: Category[] = this.categories.filter(
+      //       (cat) => cat.categoryId == +categoryId
+      //     );
+      //     this.selectedCategories = this.selectedCategories.concat(temp);
+      //     console.log('SELECTED CATEGORY: ' + this.selectedCategories);
+      //   });
+      // });
+    });
+  }
+
+  onCategoryChange(event: any, category?: Category) {
+    let extractedCategoryIds = this.selectedCategories.map((a) => a.categoryId);
+    this.router.navigate(['/viewAllProducts'], {
+      queryParams: {
+        category: extractedCategoryIds,
+      },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  fetchTags() {
+    this.standardProductService.getTags().subscribe((response) => {
+      // console.log('TAGS: ' + respond);
+      for (let object of response) {
+        this.tags.push(object as Tag);
+      }
+      this.tags = response;
+      // console.log('TAGS: ' + this.tags);
+      // let selectedTagIds =
+      //   this.activatedRoute.snapshot.queryParamMap.getAll('tag');
+
+      // if (selectedTagIds) {
+      //   selectedTagIds.forEach((tagId) => {
+      //     const temp: Tag[] = this.tags.filter((b) => b.tagId == +tagId);
+      //     this.selectedTags = this.selectedTags.concat(temp);
+      //   });
+      // }
+    });
+  }
+
+  onTagChange(event: any) {
+    let extractedTagIds = event.value.map((tag: Tag) => tag.tagId);
+    this.router.navigate(['/viewAllProducts'], {
+      queryParams: {
+        tag: extractedTagIds,
+      },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  onPriceChange(event: any) {
+    console.log(event.values);
+    console.log(this.selectedPriceRange);
+    this.router.navigate(['/viewAllProducts'], {
+      queryParams: {
+        priceMin: event.values[0],
+        priceMax: event.values[1],
+      },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  setPriceRange() {
+    let min = this.activatedRoute.snapshot.queryParamMap.get('priceMin');
+    let max = this.activatedRoute.snapshot.queryParamMap.get('priceMax');
+
+    if (min && max) {
+      this.selectedPriceRange = [0, 1500];
+      this.selectedPriceRange = [+min, +max];
+    }
+  }
 }
--- src/app/products/view-all-products/view-all-products.component.ts
+++ src/app/products/view-all-products/view-all-products.component.ts
@@ -1,15 +1,144 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { PrimeNGConfig, SelectItem } from 'primeng/api';
+import { StandardProduct } from 'src/app/models/standard-product';
+import { StandardProductService } from 'src/app/services/standard-product.service';
+import { Category } from 'src/app/models/category';
+import { Tag } from 'src/app/models/tag';
 @Component({
   selector: 'app-view-all-products',
   templateUrl: './view-all-products.component.html',
-  styleUrls: ['./view-all-products.component.css']
+  styleUrls: ['./view-all-products.component.css'],
 })
 export class ViewAllProductsComponent implements OnInit {
+  standardProducts: StandardProduct[];
+  sortOptions: SelectItem[];
+  sortKey: string = '';
+  sortField: string = '';
+  sortOrder: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private standardProductService: StandardProductService,
+    private primengConfig: PrimeNGConfig,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.standardProducts = new Array();
+    this.sortOptions = [
+      { label: 'Price High to Low', value: '!unitPrice' },
+      { label: 'Price Low to High', value: 'unitPrice' },
+    ];
   }
 
+  ngOnInit(): void {
+    console.log('********** ViewAllProductsComponent.ts: NGONIT');
+    this.activatedRoute.queryParams.subscribe((params) => {
+      console.log(params);
+      let keyword: String | undefined = params['keyword'];
+      let categoryIds: number[] | undefined = params['category'];
+      let tagIds: number[] | undefined = params['tag'];
+      if (categoryIds && !Array.isArray(categoryIds)) {
+        categoryIds = new Array(categoryIds);
+      }
+      if (tagIds && !Array.isArray(tagIds)) {
+        tagIds = new Array(tagIds);
+      }
+
+      this.fetchProducts(keyword, categoryIds, tagIds);
+      console.log('********** ViewAllProductsComponent.ts: IN ACTIVATED ROUTE');
+      console.log('catid count = ' + categoryIds);
+    });
+  }
+
+  fetchProducts(keyword?: String, categoryIds?: number[], tagIds?: number[]) {
+    console.log('********** ViewAllProductsComponent.ts: FETCH');
+    this.standardProductService.getStandardProducts().subscribe((response) => {
+      this.standardProducts = response.filter((standardProduct) => {
+        if (
+          standardProduct.quantityInStock &&
+          standardProduct.quantityInStock > 0
+        ) {
+          console.log(
+            '********** ViewAllProductsComponent.ts: FIRST FILTER ' +
+              standardProduct.productId
+          );
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      console.log('********** ViewAllProductsComponent.ts: AFTER RESPONSE');
+
+      this.standardProducts = this.standardProducts.filter(
+        (standardProduct) => {
+          let categoryShow: boolean | undefined = true;
+          let tagShow: boolean | undefined = true;
+          //   if (keyword) {
+          //     searchShow = standardProduct.name
+          //       ?.toLowerCase()
+          //       .includes(keyword?.toLowerCase());
+          //   }
+
+          if (categoryIds) {
+            for (let categoryId of categoryIds) {
+              console.log('Catid = ' + categoryId);
+              categoryShow = standardProduct.category?.categoryId == categoryId;
+              if (categoryShow) return true;
+              else return false;
+            }
+          } else return true;
+
+          if (tagIds) {
+            for (let tagId of tagIds) {
+              if (standardProduct.tags) {
+                for (let tag of standardProduct.tags) {
+                  tagShow = tag.tagId == tagId;
+                  if (tagShow) return true;
+                  else return false;
+                }
+              }
+            }
+          } else return true;
+
+          return false;
+        }
+      );
+      // );
+      // );
+    });
+  }
+  // }
+  // };
+
+  //           if (tagIds) {
+  //             for (let tagId of tagIds) {
+  //               if (standardProduct.tags) {
+  //                 for (let tag of standardProduct.tags) {
+  //                   tagShow = tag.tagId == tagId;
+  //                   if (tagShow) break;
+  //                 }
+  //               }
+  //             }
+  //           }
+  //         }
+  //       );
+
+  //       // });
+  //     },
+  //     (error) => {
+  //       console.log('********** ViewAllProductsComponent.ts: ' + error);
+  //     }
+  //   );
+  // }
+  onSortChange(event: any) {
+    let value = event.value;
+
+    if (value.indexOf('!') === 0) {
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    } else {
+      this.sortOrder = 1;
+      this.sortField = value;
+    }
+  }
 }
